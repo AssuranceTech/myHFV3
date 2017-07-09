e83ce4e2059ed55f31056d19e2f864b9fd8864af
AssuranceTech <arshad.zia@gmail.com>
2017-07-09 23:16:09 +0100

    Added comment

--- src/test/java/com/org/myHFV3/TestBrowser.java
+++ src/test/java/com/org/myHFV3/TestBrowser.java
@@ -10,7 +10,7 @@ public class TestBrowser {
 	
 	@Test
 	public void OpenBrowser() throws IOException {
-		System.out.println("Opening Browser"):
+		System.out.println("Opening Browser");
 		WebDriver driver;		
 		System.setProperty("webdriver.gecko.driver", "C://Users//asyedzia//workspace//myHFV3//geckodriver.exe");
 		driver = new FirefoxDriver();
